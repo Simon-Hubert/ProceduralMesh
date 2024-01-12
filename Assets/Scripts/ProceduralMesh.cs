37a09db501156103157653b774eab6e9a4cc855b
Simon-Hubert <s.hubert.mailpro@gmail.com>
2024-01-12 13:11:29 +0100

    Procedural SquareGrid

--- Assets/Scripts/ProceduralMesh.cs
+++ Assets/Scripts/ProceduralMesh.cs
@@ -1,39 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using ProceduralMeshes;
+using ProceduralMeshes.Generators;
+using ProceduralMeshes.Streams;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class ProceduralMesh : MonoBehaviour
 {
-    private void OnEnable() {
-        var mesh = new Mesh {
-            name = "ProceduralMesh"
-        };
+    Mesh mesh;
 
-        mesh.vertices = new Vector3[] {
-            Vector3.zero, Vector3.right, Vector3.up, new Vector3(1f, 1f) 
+    private void Awake() {
+        mesh = new Mesh{
+            name = "Procedural Mesh"
         };
-
-        mesh.triangles = new int[]{
-            0, 2, 1, 1, 2, 3
-        };
-
-        mesh.normals = new Vector3[] {
-            Vector3.back, Vector3.back, Vector3.back, Vector3.back
-        };
-
-        mesh.uv = new Vector2[] {
-            Vector2.zero, Vector2.right, Vector2.up, Vector2.one
-        };
-
-        mesh.tangents = new Vector4[]{
-            new Vector4(1f, 0f, 0f, -1f),
-            new Vector4(1f, 0f, 0f, -1f),
-            new Vector4(1f, 0f, 0f, -1f),
-            new Vector4(1f, 0f, 0f, -1f)
-        };
-
+        GenerateMesh();
         GetComponent<MeshFilter>().mesh = mesh;
     }
-    
+
+    private void GenerateMesh()
+    {
+        Mesh.MeshDataArray meshDataArray = Mesh.AllocateWritableMeshData(1);
+        Mesh.MeshData meshData = meshDataArray[0];
+
+        MeshJob<SquareGrid, SingleStream>.ScheduleParallel(meshData, default).Complete();
+
+        Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
+    }
 }
---
+++ Assets/Scripts/ProceduralMesh/Streams/SingleStream.cs
@@ -0,0 +1,63 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
+
+namespace ProceduralMeshes.Streams {
+    public struct SingleStream : IMeshStreams
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        struct Stream0{
+            public float3 position, normal;
+            public float4 tangent;
+            public float2 texCoord0;
+        }
+
+        [NativeDisableContainerSafetyRestriction]
+        NativeArray<Stream0> stream0;
+
+        [NativeDisableContainerSafetyRestriction]
+        NativeArray<int3> triangles;
+
+        public void Setup(Mesh.MeshData data, int vertexCount, int indexCount)
+        {
+            var descriptor = new NativeArray<VertexAttributeDescriptor>(
+                4, Allocator.Temp, NativeArrayOptions.UninitializedMemory
+            );
+
+            descriptor[0] = new VertexAttributeDescriptor(dimension: 3);
+            descriptor[1] = new VertexAttributeDescriptor(VertexAttribute.Normal, dimension: 3);
+            descriptor[2] = new VertexAttributeDescriptor(VertexAttribute.Tangent, dimension: 4);
+            descriptor[3] = new VertexAttributeDescriptor(VertexAttribute.TexCoord0, dimension: 2);
+
+            data.SetVertexBufferParams(vertexCount, descriptor);
+            descriptor.Dispose();
+
+            data.SetIndexBufferParams(indexCount, IndexFormat.UInt32);
+
+            data.subMeshCount = 1;
+            data.SetSubMesh(0, new SubMeshDescriptor(0, indexCount),
+                MeshUpdateFlags.DontRecalculateBounds |
+                MeshUpdateFlags.DontValidateIndices
+            );
+
+            stream0 = data.GetVertexData<Stream0>();
+            triangles = data.GetIndexData<int>().Reinterpret<int3>(4);
+        }
+
+        public void SetTriangle(int index, int3 triangle) => triangles[index] = triangle;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetVertex(int index, Vertex data) => stream0[index] = new Stream0
+        {
+            position = data.position,
+            normal = data.normal,
+            tangent = data.tangent,
+            texCoord0 = data.texCoord0
+        };
+    }
+}
